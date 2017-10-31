f96cd11416f8498cfa5afb13a34e656220a9f986
alexg1t <33228861+alexg1t@users.noreply.github.com>
2017-10-31 10:15:28 -0500

    Update memory.vhd

--- memory.vhd
+++ memory.vhd
@@ -52,13 +52,13 @@ signal data_mem: mem_array := (
 
 begin
 
-read_data <= data_mem(conv_integer(address(6 downto 2))) when MemRead = '1' else X"00000000";
+read_data <= data_mem(conv_integer(address(4 downto 0))) when MemRead = '1' else X"00000000";
 
 mem_process: process(address, write_data,clock)
 begin
 	if clock = '0' and clock'event then
 		if (MemWrite = '1') then
-			data_mem(conv_integer(address(4 downto 0))) <= write_data;
+			data_mem(conv_integer(address(6 downto 2))) <= write_data;
 		end if;
 	end if;
 end process mem_process;
